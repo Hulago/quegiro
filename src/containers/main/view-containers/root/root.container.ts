476b7b092217e18323e2c81bcde6f2bc795b01e1
Hugo Rodrigues <hugorodrigues@prozis.com>
2022-05-31 16:22:19 +0100

    (feat) add vue-meta

--- src/containers/main/view-containers/root/root.container.ts
+++ src/containers/main/view-containers/root/root.container.ts
@@ -3,7 +3,13 @@ import Vue from 'vue';
 
 // TODO set locale from store
 @Component({
-  name: 'root-container'
+  name: 'root-container',
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: 'Quegiro',
+    // all titles will be injected into this template
+    titleTemplate: '%s | Quegiro'
+  }
 })
 export default class extends Vue {
   // force rerender when locale change
